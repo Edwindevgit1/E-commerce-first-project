87da15ebf3cdd5355e620531bf020a3f2079b4e9
Edwin Babu <edwinbabupullazhi06@gmail.com>
2026-03-12 22:49:02 +0530

    update

--- routes/user/signupotp.route.js
+++ routes/user/signupotp.route.js
@@ -9,22 +9,16 @@ import signupUser, {
 
 const router = express.Router();
 
-// signup page
 router.get("/register", getSignupPage);
 
-// OTP page
 router.get("/signupotp", getSignupOtpPage);
 
-// cancel signup
 router.get("/cancel-signup", cancelSignup);
 
-// submit signup form
 router.post("/signupotp", signupUser);
 
-// resend OTP
 router.post("/resend-otp", resendSignupOtp);
 
-// verify OTP
 router.post("/signupverify-otp", verifySignupOtp);
 
 export default router;
